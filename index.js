c1dbfc3bbedca6bdf9d2eb64bcda3c75aaf98c3d
Rita-mary Ngozi Amakom <amakomritamary322@gmail.com>
2022-06-29 23:16:00 +0200

    I learnt how to use switch in functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 console.log('Hello my friend, \n this message and its meaning \n \t was formatted using "Javascript"')
+// var x = 4;
+// if (x < 4) {
+//     console.log("X is less than 4")
+// }else if (x == 4){
+//     console.log("X is equal to 4")
+// } else {
+//     console.log("X is greater than 4")
+// }
+//alert("This life is good")
+var myArray = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Friday"];
+// myArray.push("Thursday");
+// myArray.shift();
+// myArray.unshift("Sunday")
+// myArray.pop()
+myArray.push[4] = "Thursday"
+console.log(myArray)
+function addition(a , b){
+    var x = a+b;
+    console.log(x);
+}
+addition(3 , 6)
+addition(4 , 89)
+function caseInswitch(num){
+    switch(num){
+        case 1:
+            console.log("The number is 1")
+            break
+        case 5:
+            console.log("The number is 5")
+            break
+        case 10:
+            console.log("The number is 10")
+            break
+    }
+}
+g = caseInswitch(10)
